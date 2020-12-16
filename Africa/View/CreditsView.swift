fe4addb4209cac0b80dc5dc849f2d368b9219a8b
berkekandemir <33724354+berkekandemir@users.noreply.github.com>
2020-12-17 01:34:36 +0300

    Add files via upload

---
+++ Africa/View/CreditsView.swift
@@ -0,0 +1,37 @@
+//
+//  CreditsView.swift
+//  Africa
+//
+//  Created by Berke Can Kandemir on 14.12.2020.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    var body: some View {
+        VStack {
+            Image("compass")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 128, height: 128)
+            
+            Text("""
+    Copyright © Berke Can Kandemir
+    All rights reserved
+    Better App ♡ Less Code
+    """)
+                .font(.footnote)
+                .multilineTextAlignment(.center)
+        } //: VSTACK
+        .padding()
+        .opacity(0.4)
+    }
+}
+
+struct CreditsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreditsView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
